27878e167f915b8f6422e99938c6b317b0358eb8
Hamza RAFIQUE <hamza>
2024-03-26 16:39:42 +0500

    first calculator

---
+++ main.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+    {message:"Enter Please Firat Number",type:"number",name:"firstnumber"},
+    {message:"Enter Please Second Number",type:"number",name:"secondnumber"},
+    {
+    message:"select one operator to perform operation",
+    name:"operator",
+    type:"list",
+    choices:["Addition","Subtraction","Multiplication","Division"],
+    }
+]);
+
+console.log(answer)
+
+
+if(answer.operator === "Addition"){
+    console.log(answer.firstnumber + answer.secondnumber)
+}
+else if(answer.operator === "Subtraction"){
+    console.log(answer.firstnumber - answer.secondnumber)
+}
+else if(answer.operator === "Multiplication"){
+    console.log(answer.firstnumber * answer.secondnumber)
+}
+else if(answer.operator === "Division"){
+    console.log(answer.firstnumber / answer.secondnumber)
+}
+else{
+    console.log("PLEASE ENTER VALID NUMBER");
+    
+}
